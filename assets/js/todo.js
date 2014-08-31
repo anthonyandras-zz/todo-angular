314d42824fc2d62e3b030f5525eb54176cc00a9b
anthonyandras <anthony.andras@gmail.com>
2014-08-31 08:54:13 -0500

    bind item count to the number of items that are not marked as done

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -14,5 +14,13 @@ items: [
 var todoApp = angular.module("todoApp", new Array());
 //create a controller for our application
 todoApp.controller("ToDoCtrl", function($scope){
-$scope.todo = model;
+  $scope.todo = model;
+
+  $scope.incompleteCount = function() {
+    var count = 0;
+    angular.forEach($scope.todo.items, function(item) {
+      if(!item.done) { count++ }
+    });
+    return count;
+  };
 });	
